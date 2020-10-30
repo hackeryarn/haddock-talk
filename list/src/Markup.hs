58dc345854383d3924280273f14ade38c222785d
artem <artem@horizoninvestments.com>
2020-10-29 20:17:40 -0500

    Started a markup module

---
+++ list/src/Markup.hs
@@ -0,0 +1,22 @@
+module Markup
+  (
+  -- * Code Blocks
+  -- $codeblocks
+  )
+where
+
+-- $codeblocks
+-- To interpret code litteraly:
+--
+-- > Prefixed the line with a \>
+--
+-- To allow Haddock markup in your code:
+--
+-- @
+-- Surrounded by \@ on new lines
+-- @
+--
+-- To show shell interaction:
+--
+-- >>> Prefix the input with >>>
+-- And just add the output on a new line
